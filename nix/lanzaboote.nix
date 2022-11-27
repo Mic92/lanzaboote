67f58a77a4cebbd765fd2c3f0e81158f786ce93e
Julian Stecklina <js@alien8.de>
2022-11-27 12:02:28 +0100

    Add thinklmi and lockdown patches

--- nix/lanzaboote.nix
+++ nix/lanzaboote.nix
@@ -1,4 +1,4 @@
-{ lib, config, pkgs, ... }: 
+{ lib, config, pkgs, ... }:
 with lib;
 let
   cfg = config.boot.lanzaboote;
@@ -11,6 +11,10 @@ in
     enable = mkEnableOption "Enable the LANZABOOTE";
 
     enrollKeys = mkEnableOption "Automatic enrollment of the keys using sbctl";
+
+    # To get that elusive green checkmark from GNOME Device Security /
+    # fwupdtool security.
+    lockdown = mkEnableOption "Security Lockdown";
 
     pkiBundle = mkOption {
       type = types.nullOr types.path;
@@ -49,7 +53,7 @@ in
           cp -r ${cfg.pkiBundle}/* /tmp/pki
           ${sbctlWithPki}/bin/sbctl enroll-keys --yes-this-might-brick-my-machine
         ''}
-  
+
         ${cfg.package}/bin/lanzatool install \
           --pki-bundle ${cfg.pkiBundle} \
           --public-key ${cfg.publicKeyFile} \
@@ -58,5 +62,20 @@ in
           /nix/var/nix/profiles/system-*-link
       '';
     };
+
+    boot.kernelPatches = mkIf cfg.lockdown [
+      {
+        # The thinklmi driver is violating the sysfs spec and fwupd
+        # really wants its 'type' field in the firmware attributes.
+        name = "think-lmi-fwupd-compat";
+        patch = ./patches/linux/0001-platform-x86-think-lmi-expose-type-attribute.patch;
+
+        # TODO Userspace must still enable lockdown via /sys/kernel/security/lockdown or via commandline
+        extraConfig = ''
+          SECURITY_LOCKDOWN_LSM y
+          SECURITY_LOCKDOWN_LSM_EARLY y
+        '';
+      }
+    ];
   };
 }
